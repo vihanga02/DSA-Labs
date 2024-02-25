c9143cc38354d56b7b83578abd56a255ec1b0e38
vihanga_02 <muthumalavdw.22@uom.lk>
2024-02-25 11:31:12 +0530

    add comments and edit messege

--- week5/in-class/array_linkedList.cpp
+++ week5/in-class/array_linkedList.cpp
@@ -4,90 +4,105 @@
  
 using namespace std;
 
-class Node{
-    private:
-        int nodeData;
-    public:
-        Node *next;
-        Node(int data){
-            this->nodeData = data;
-            this->next = nullptr;
-        }
-        
-        int data(){
-            return nodeData;
-        }
+class Node {
+private:
+    int nodeData;
+
+public:
+    Node *next;
+
+    // Constructor to initialize Node with data
+    Node(int data) {
+        this->nodeData = data;
+        this->next = nullptr;
+    }
+    
+    // Method to get data of Node
+    int data() {
+        return nodeData;
+    }
 };
 
-class  LinkedStack
-{
+class LinkedStack {
 private:
     int length;
+
 public:
     Node *top;
-    LinkedStack(){
+
+    // Constructor to initialize LinkedStack
+    LinkedStack() {
         top = nullptr;
         this->length = 0;
     }
 
-    void push(int data){
+    // Method to push data onto the stack
+    void push(int data) {
         Node *newNode = new Node(data);
-        if (isEmpty()){
+        if (isEmpty()) {
+            // If the stack is empty, set the new node as top
             top = newNode;
-        }
-        else
-        {
+        } else {
+            // Otherwise, insert the new node at the top and update top pointer
             newNode->next = top;
             top = newNode; 
         }
+        // Increment the length of the stack
         length++;
     }
 
-    int pop(){
-        if (isEmpty()){
+    // Method to pop data from the stack
+    int pop() {
+        if (isEmpty()) {
+            // If the stack is empty, throw underflow error
             throw underflow_error("Stack is Empty");
-        }
-        else{
+        } else {
+            // Otherwise, remove the top node and update top pointer
             Node *currentNode = top;
             top = top->next;
+            // Decrement the length of the stack
             length--;
+            // Return the data of the removed node
             return currentNode->data();
         }
     }
 
-    bool isEmpty(){
-        if (top == nullptr){
-            return true;
-        }
-        else{
-            return false;
-        }
+    // Method to check if stack is empty
+    bool isEmpty() {
+        // Check if top pointer is null
+        return top == nullptr;
     }
 
-    int stackTop(){
-        if (isEmpty()){
+    // Method to get the top element of the stack
+    int stackTop() {
+        if (isEmpty()) {
+            // If the stack is empty, throw underflow error
             throw underflow_error("Stack is Empty");
-        }
-        else{
+        } else {
+            // Otherwise, return the data of the top node
             return this->top->data();
         }
     }
 
-    void display(){
+    // Method to display the stack elements
+    void display() {
         Node *currentNode = this->top;
-        while (currentNode != nullptr){
-            cout << currentNode->data() << " " ;
+        while (currentNode != nullptr) {
+            // Iterate through the stack and print data of each node
+            cout << currentNode->data() << " ";
             currentNode = currentNode->next;
         }
+        // Print newline character after displaying all elements
         cout << endl;
     }
 };
 
-int main(){
-    try
-    {
+int main() {
+    try {
         LinkedStack stack;
         auto startingTime = chrono::high_resolution_clock::now();
+        
+        // Pushing elements onto the stack
         stack.push(8);
         stack.push(10);
         stack.push(10);
@@ -99,23 +114,38 @@ int main(){
         stack.push(18);
         stack.push(20);
         stack.push(17);
+        
+        // Displaying the stack
         stack.display();
+        
+        // Popping elements from the stack
         stack.pop();
         stack.pop();
         stack.pop();
         stack.pop();
         stack.pop();
+        
+        // Displaying the stack after popping
         stack.display();
+        
+        // Pushing more elements onto the stack
         stack.push(4);
         stack.push(30);
         stack.push(3);
         stack.push(1);
+        
+        // Displaying the stack after pushing
         stack.display();
+        
         auto endingTime = chrono::high_resolution_clock::now();
-        double totale_time = chrono::duration_cast<chrono::microseconds>(endingTime - startingTime).count();
-        cout << totale_time << endl;
+        double total_time = chrono::duration_cast<chrono::microseconds>(endingTime - startingTime).count();
+        
+        // Printing the total execution time
+        cout << "Total execution time: " << total_time << " microseconds" << endl;
     }
-    catch (exception& e){
-        cerr << endl <<  "Exception: " << e.what() << endl;
+    catch (exception& e) {
+        // Handling exceptions and printing error messages
+        cerr << "Exception: " << e.what() << endl;
     }
+    return 0;
 }
--- week5/in-class/array_stack.cpp
+++ week5/in-class/array_stack.cpp
@@ -10,60 +10,69 @@ private:
     int top;
     int stack_size;
     int arr[];
+
 public:
+    // Constructor to initialize array_stack with specified size
     array_stack(int size){
         this->stack_size = size;
+        // Allocate memory for array based on size
         this->arr[size];
         this->top = -1;
     }
     
+    // Method to push data onto the stack
     void push(int newData){
         this->top++;
         if (top > stack_size){
+            // Throw overflow error if stack is full
             throw overflow_error("Stack is full");
         }
         else{
+            // Insert new data into array at top index
             arr[top] = newData;
         }
     }
     
+    // Method to pop data from the stack
     int pop(){
         if (top == -1){
+            // Throw overflow error if stack is empty
             throw overflow_error("Stack is empty");
         }
         else{
+            // Remove and return data from top of the stack
             int previousTop =  arr[top];
             top--;
             return previousTop;
         }
     }
+
+    // Method to check if stack is empty
     bool isEmpty(){
-        if (top == -1){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return top == -1;
     }
+
+    // Method to check if stack is full
     bool isFull(){
-        if (top + 1 == stack_size){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return top + 1 == stack_size;
     }
+
+    // Method to display the stack elements
     void display(){
         if (top == -1){
+            // Print message if stack is empty
             cout << "stack is empty" << endl;
         }
         else{
+            // Print all elements of the stack
             for (int i = 0; i < stack_size; i++){
                 cout << arr[i] << " ";
             }
             cout << endl;
         }
     }
+
+    // Method to get the top element of the stack
     int StackTop(){
         return arr[top];
     }
@@ -78,6 +87,8 @@ int main(){
     {
         array_stack stack(stack_size);
         auto startingTime = chrono::high_resolution_clock::now();
+        
+        // Pushing elements onto the stack
         stack.push(8);
         stack.push(10);
         stack.push(10);
@@ -89,23 +100,35 @@ int main(){
         stack.push(18);
         stack.push(20);
         stack.push(17);
+        
+        // Displaying the stack
         stack.display();
+        
+        // Popping elements from the stack
         stack.pop();
         stack.pop();
         stack.pop();
         stack.pop();
         stack.pop();
+        
+        // Displaying the stack after popping
         stack.display();
+        
+        // Pushing more elements onto the stack
         stack.push(4);
         stack.push(30);
         stack.push(3);
         stack.push(1);
+        
+        // Displaying the stack after pushing
         stack.display();
+        // Display total execution time
         auto endingTime = chrono::high_resolution_clock::now();
-        double totale_time = chrono::duration_cast<chrono::microseconds>(endingTime - startingTime).count();
-        cout << totale_time << endl;
+        double total_time = chrono::duration_cast<chrono::microseconds>(endingTime - startingTime).count();
+        cout << "Total execution time: " << total_time << " microseconds" << endl;
     }
     catch (exception& e){
+        // Handle exceptions
         cerr << endl <<  "Exception: " << e.what() << endl;
     }
 }
