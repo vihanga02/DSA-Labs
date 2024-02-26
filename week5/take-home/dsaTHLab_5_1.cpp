39e613561ced9c888a53af69b13843c2d51623a6
vihanga_02 <muthumalavdw.22@uom.lk>
2024-02-27 01:34:13 +0530

    Completed take home 1 and completed take home 2 for a single input

--- week5/take-home/dsaTHLab_5_1.cpp
+++ week5/take-home/dsaTHLab_5_1.cpp
@@ -1,1 +1,69 @@
 #include <iostream>
+
+using namespace std;
+
+class Node
+{
+private:
+    int nodeData;
+public:
+    Node *next;
+    Node(int data){
+        this->nodeData = data;
+        this->next = nullptr;
+    }
+
+    int data(){
+        return nodeData;
+    }
+};
+
+class LinkedList{
+public:
+    Node *head;
+    Node *root;
+    LinkedList(){
+        this->head = nullptr;
+    }
+
+    void push(int data){
+        if (this->head == nullptr) {
+            this->head = new Node(data);
+        } else {
+            Node *current = this->head;
+            while (current->next != nullptr) {
+                current = current->next;
+            }
+            current->next = new Node(data);
+        }
+    }
+};
+
+void reversePrint(Node* node){
+    if (node == nullptr){
+        return;
+    }
+    reversePrint(node->next);
+    cout << node->data() << endl;
+}
+
+int main(){
+    int listCount;
+    cin >> listCount;
+    LinkedList *arr[listCount];
+    for (int i = 0; i < listCount; i++){
+        LinkedList *list = new LinkedList();
+        arr[i] = list;
+        int listLength;
+        cin >> listLength;
+        for (int j = 0; j < listLength; j++){
+            int num;
+            cin >> num;
+            arr[i]->push(num);
+        }
+    }
+
+    for (int k = 0; k < listCount; k++){
+        reversePrint(arr[k]->head);
+    }
+}
