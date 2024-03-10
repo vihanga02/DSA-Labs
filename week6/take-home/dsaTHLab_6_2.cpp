cd2e1a796a0f02a082596d44058da6974d83ad8e
vihanga_02 <muthumalavdw.22@uom.lk>
2024-03-10 10:03:22 +0530

    week 6 update

--- week6/take-home/dsaTHLab_6_2.cpp
+++ week6/take-home/dsaTHLab_6_2.cpp
@@ -1,24 +1,31 @@
+// Include the necessary libraries for input/output and standard template library
 #include <bits/stdc++.h>
 
+// Use the standard namespace
 using namespace std;
 
+// Define a class for representing a node in a binary tree
 class Node {
     public:
-    int data;
-    Node *left;
-    Node *right;
-    Node (int d){
-        data = d;
+    // Data stored in the node
+    int data; 
+    // Pointer to the left child node and right
+    Node *left; 
+    Node *right; 
+    // Constructor to initialize the node with data
+    Node (int d){ 
+        data = d; 
         left = NULL;
-        right = NULL;
+        right = NULL; 
     }
 };
 
+// Define a class for the solution
 class Solution {
     public:
-    // Insert a node
+    // Function to insert a node into the binary tree
     Node *insertNode(Node *node, int data) {
-        // If the tree is empty, create a new node with the given key and assign the new node as the node
+        // If the tree is empty, create a new node with the given data and set it as the root
         if (node == NULL){
             Node * newNode = new Node(data);
             newNode->data = data;
@@ -26,17 +33,17 @@ class Solution {
             newNode->right = NULL;
             node = newNode;
         }
-        // If the key is greater than the current node's key, insert it into the right subtree
+        // If the data is greater than the current node's data, insert it into the right subtree
         else if (node->data < data)
         {
             node->right = insertNode(node->right, data);
         }
-        // If the key is smaller than the current node's key, insert it into the left subtree
+        // If the data is smaller than the current node's data, insert it into the left subtree
         else if (node->data > data)
         {
             node->left = insertNode(node->left, data);
         }
-         // If the key is equal the current node's key, insert it into the right subtree
+         // If the data is equal to the current node's data, insert it into the right subtree
         else{
             node->right = insertNode(node->right, data);
         }
@@ -44,75 +51,52 @@ class Solution {
         return node;
     }
 
-    void lca (Node *node, int v1, int v2){
+    // Function to find the Lowest Common Ancestor (LCA) of two nodes in a binary tree
+    bool lca(Node *node, int v1, int v2){
         int nodeData;
         
-        if (node == NULL){
-            return ;
-        }
-        nodeData = node->data;
-
-        // if ((v1 <= nodeData && nodeData <= v2)||(v1 >= nodeData && nodeData >= v2)){
-        //     // if (elementFound(node->left, min(v1,v2)) && elementFound(node->right, max(v1,v2))){
-        //     //     cout << nodeData << endl;
-        //     //     return true;
-        //     // }
-        //     // else{
-        //     //     return false;
-        //     // }
-        //     cout << nodeData << endl;
-        // }
-        if ((v1 < nodeData && nodeData > v2)){
-             lca(node->left, v1, v2);
-        }
-        if ((v1 > nodeData && nodeData < v2)){
-             lca(node->right, v1, v2);
-        }
-        cout << nodeData << endl;
-        return;
-    }
-
-    bool elementFound(Node *node, int v){
+        // If the node is NULL, return false
         if (node == NULL){
             return false;
         }
-        if (node->data == v){
-           return true;
-        }
-        
-        if (v < node->data) {
-            return elementFound(node->left, v);
-        }
-        else{
-            return elementFound(node->right, v);
-        }
+        nodeData = node->data;
 
+        // If both v1 and v2 are less than the current node's data, search in the left subtree
+        if ((v1 < nodeData && nodeData > v2)){
+             return lca(node->left, v1, v2);
+        }
+        // If both v1 and v2 are greater than the current node's data, search in the right subtree
+        if ((v1 > nodeData && nodeData < v2)){
+             return lca(node->right, v1, v2);
+        }
+        // Print the current node's data and return true
+        cout << nodeData << endl;
+        return true;
     }
 };
 
+// Main function
 int main() {
-    Solution myTree;
-    Node *root= NULL;
-    int t, data, v1, v2;
-    cin >> t;
+    // Create an instance of the Solution class
+    Solution myTree; 
+    // Initialize the root node as NULL
+    Node *root= NULL; 
+    // Declare variables for input
+    int t, data, v1, v2; 
+    // Read the number of nodes in the tree
+    cin >> t; 
+    // Loop to read the data of each node and insert it into the tree
     for (int i= 0; i<t; i++){
-        cin >> data;
-        root = myTree.insertNode(root,data);
+        // Read the data of the current node and insert into the tree
+        cin >> data; 
+        root = myTree.insertNode(root,data); 
     }
-    cin >> v1 >> v2 ;
-    // if (!myTree.lca(root,v1,v2)){
-    //     cout << root->data << endl;
-    // }
-     myTree.lca(root, v1, v2);
-    return 0;
+    // Read the values of v1 and v2
+    cin >> v1 >> v2 ; 
+
+    // Call the function to find the Lowest Common Ancestor and print the root data if return false from lca function
+    if (!myTree.lca(root, v1, v2)){
+        cout << root->data << endl;
+    }
+    return 0; // Return 0 to indicate successful execution
 }
-
-/* 
-6
-4 2 3 1 7 6
-1 7
-
-4005,4103,4212,4330,4435,4506,4648,4767,4879,4913,4008,4135,4263,4319,4444,4556,4634,4715,4840,4975,4022,4126,4237,4314,4422,4519,4618,4737,4821,4938,4033,4154,4249,4339,4455,4567,4647,4751,4869,4986
-4115 4426
-
-*/
---
+++ week7/in-class/heap.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+  
+// function to heapify the tree
+void heapify(int arr[], int n, int root)
+{
+   // build heapify  
+   int maximumValueIndex = root;
+   int leftChildIndIex = 2*root + 1;
+   int rightChildndex = 2*root + 2;
+1
+   if (leftChildIndex < n && arr[root] < arr[leftChildIndex]){
+      maximumValueIndex = leftChildIndIex;
+   }
+   
+   if (rightChildIndex < n && arr[root-1] < arr[rightChildIndex-1]){
+      maximumValueIndex = rightChildndex; 
+   }
+
+   if (maximumValueIndex != root){
+      swap(arr[root], arr[maximumValueIndex]);
+      heapify(arr, n, maximumValueIndex);
+   }
+
+}
+  
+// implementing heap sort
+void heapSort(int arr[], int n)
+{
+   // build heap
+   for (int i = n/2-1; i >= 0; i--){
+      heapify(arr,n,i);
+   }
+  
+   // extracting elements from heap one by one
+   for (int j = n-1; j > 0 1; j--){
+      swap(arr[0], arr[i]);
+      heapify(arr, i, 0);
+   }
+}
+  
+/* print contents of array */
+void displayArray(int arr[], int n)
+{
+   for (int i=0; i<n; ++i)
+   cout << arr[i] << " ";
+   cout << "\n";
+}
+  
+// main program
+int main()
+{
+   int heap_arr[] = {4,17,3,12,9,6};
+   int n = sizeof(heap_arr)/sizeof(heap_arr[0]);
+   cout<<"Input array"<<endl;
+   displayArray(heap_arr,n);
+  
+   heapSort(heap_arr, n);
+  
+   cout << "Sorted array"<<endl;
+   displayArray(heap_arr, n);
+}
